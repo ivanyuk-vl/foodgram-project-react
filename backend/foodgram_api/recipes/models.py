6017a586107f83f303418f754028f6a8d6b1ec32
Ivanyuk Vladislav <ivanyuk.vl@yandex.ru>
2022-06-03 17:03:36 +1000

    add Recipe model

--- backend/foodgram_api/recipes/models.py
+++ backend/foodgram_api/recipes/models.py
@@ -1,27 +1,68 @@
 from django.db import models
 
+from users.models import User
+
 INGREDIENT_STR = 'id: {}, название: {}, ед.изм.: {}'
 TAG_STR = 'id:{}, название: {}, цвет: {}, метка: {}'
 
 
 class Ingredient(models.Model):
-    name = models.CharField('название', max_length=254, blank=False)
-    measurement_unit = models.CharField(
-        'единица измерения', max_length=254, blank=False
-    )
+    name = models.CharField('название', max_length=254, unique=True)
+    measurement_unit = models.CharField('единица измерения', max_length=254)
 
     def __str__(self):
         return INGREDIENT_STR.format(
             self.pk, self.name[:15], self.measurement_unit[:15]
         )
 
+    class Meta:
+        verbose_name = 'ингредиент'
+        verbose_name_plural = 'ингредиенты'
+
+
+class IngredientAmount(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.IntegerField('количество')
+
+    class Meta:
+        verbose_name = 'количество ингредиента'
+        verbose_name_plural = 'количества ингредиентов'
+
+
+class IngredientAmountRecipe(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    ingredient_amount = models.ForeignKey(
+        IngredientAmount, on_delete=models.CASCADE
+    )
+
 
 class Tag(models.Model):
-    name = models.CharField(
-        'название', max_length=254, blank=False, unique=True
-    )
-    color = models.CharField('цвет', max_length=7, blank=False, unique=True)
-    slug = models.SlugField('метка', blank=False, unique=True)
+    name = models.CharField('название', max_length=254, unique=True)
+    color = models.CharField('цвет', max_length=7, unique=True)
+    slug = models.SlugField('метка', unique=True)
 
     def __str__(self):
         return TAG_STR.format(self.pk, self.name[:15], self.color, self.slug)
+
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
+
+
+class TagRecipe(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='recipes'
+    )
+    name = models.CharField('название', max_length=200)
+    image = models.ImageField('изображение', upload_to='recipes/')
+    text = models.TextField('описание')
+    ingredients = models.ManyToManyField(
+        IngredientAmount, through='IngredientAmountRecipe'
+    )
+    tags = models.ManyToManyField(Tag, through='TagRecipe')
+    cooking_time = models.IntegerField('время приготовления')
