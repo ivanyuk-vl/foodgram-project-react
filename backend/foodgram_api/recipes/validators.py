910e45c50867f52618ab45b79ec9835a88a4b663
Ivanyuk Vladislav <ivanyuk.vl@yandex.ru>
2022-06-05 14:32:39 +1000

    edit models, validators

--- backend/foodgram_api/recipes/validators.py
+++ backend/foodgram_api/recipes/validators.py
@@ -3,14 +3,15 @@ from django.core.validators import RegexValidator
 
 HEX_COLOR_ERROR = (
     'Введите правильный цвет. Это значение должно начинаться с символа #, '
-    'за которым следует три пары шестнадцатеричных цифр.'
+    'за которым следует три пары шестнадцатеричных цифр. Пример: #8fce00.'
 )
 MIN_COOKING_TIME_ERROR = 'Время приготовления не может быть меньше 1 минуты.'
-
-
-class HexColorValidator(RegexValidator):
-    regex = r'^#[\da-fA-F]{6}\Z'
-    message = HEX_COLOR_ERROR
+SLUG_ERROR = (
+    'Значение должно состоять только из букв, цифр, знаков '
+    'подчеркивания или дефиса.'
+)
+validate_hex_color = RegexValidator(r'^#[a-fA-F0-9]{6}\Z', HEX_COLOR_ERROR)
+validate_slug = RegexValidator(r'^[-a-zA-Z0-9_]+$', SLUG_ERROR)
 
 
 def min_cooking_time_validator(time):
