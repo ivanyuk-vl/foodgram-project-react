8cf564856e8c52674e687234d405e79a433a5d14
Ivanyuk Vladislav <ivanyuk.vl@yandex.ru>
2022-06-08 17:16:13 +1000

    add CustomUserAdmin model

--- backend/foodgram_api/users/admin.py
+++ backend/foodgram_api/users/admin.py
@@ -3,4 +3,11 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = ((None, {
+        'classes': ('wide',),
+        'fields': ('username', 'email', 'first_name',
+                   'last_name', 'password1', 'password2')
+    }),)
